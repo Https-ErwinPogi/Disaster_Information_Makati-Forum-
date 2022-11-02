e4d934a59a2e2b5dc5dc907312116741d87c243f
KodaCamp <>
2022-11-02 11:40:24 +0800

    create new action and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,20 @@
 class PostsController < ApplicationController
-
+  before_action :authenticate_user!,except: [:index]
   def index
     @posts = Post.includes(:user).all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :content, :address))
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 end
