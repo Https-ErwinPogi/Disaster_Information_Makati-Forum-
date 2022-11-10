3f91025e9b12503e49a0d9217003bd003af836a6
KodaCamp <>
2022-11-10 18:42:11 +0800

    remove unnecessary path and add get params uniq id to url

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "posts#index"
 
-  resources :posts, path_names: {new: '99', edit: '88'} do
+  resources :posts do
     resources :comments, except: :show
   end
 
   resources :categories
+
+  get '/:unique_id', to: 'posts#short_url'
 end
