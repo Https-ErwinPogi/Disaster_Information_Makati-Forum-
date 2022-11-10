de6f863e3bd328fbf4214ce2d7a43576a20c481a
KodaCamp <>
2022-11-10 18:42:47 +0800

    add after validation and use loop for uniq id

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,20 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :address, presence: true
+  after_validation :generate_short_string
 
   belongs_to :user
   has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+
+  private
+
+  def generate_short_string
+    loop do
+      @unique_string = sprintf "%04d", rand(2 - 9999)
+      break unless Post.exists?(unique_id: @unique_string)
+    end
+    self.unique_id = @unique_string
+  end
 end
