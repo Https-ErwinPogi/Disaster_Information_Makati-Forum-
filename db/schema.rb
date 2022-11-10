c8c5b686a6d251b8b5aaaa2bb90dbe4e3a4f3632
KodaCamp <>
2022-11-10 17:40:24 +0800

    migrate add ip address to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_082545) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_10_082833) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "disaster"
     t.datetime "created_at", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_082545) do
     t.datetime "updated_at", null: false
     t.datetime "discarded_at"
     t.integer "comments_count", default: 0
+    t.string "unique_id"
     t.index ["discarded_at"], name: "index_posts_on_discarded_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
@@ -62,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_082545) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ip_address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
