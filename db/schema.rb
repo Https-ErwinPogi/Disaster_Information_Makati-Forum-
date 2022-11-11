0e04c7e293d05a4b892f6ab67926995424740f3c
KodaCamp <>
2022-11-11 12:58:30 +0800

    add address:region model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_10_102629) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_034006) do
+  create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "disaster"
     t.datetime "created_at", null: false
