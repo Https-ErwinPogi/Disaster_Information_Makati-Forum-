a206241b4fbca460e7820c52c7a7552748fedae8
KodaCamp <>
2022-11-11 14:33:22 +0800

    done fetch_province

--- app/services/ph_location_service.rb
+++ app/services/ph_location_service.rb
@@ -6,12 +6,24 @@ class PhLocationService
   end
 
   def fetch_region
-    request = RestClient.get("#{url}/regions/")
+    request = RestClient.get("#{url}/regions")
     data = JSON.parse(request.body)
     data.each do |region|
       address_region = Address::Region.find_or_initialize_by(code: region['code'])
       address_region.name = region['regionName']
       address_region.save
+    end
+  end
+
+  def fetch_province
+    request = RestClient.get("#{url}/provinces")
+    data = JSON.parse(request.body)
+    data.each do |province|
+      address_province = Address::Province.find_or_initialize_by(code: province['code'])
+      region = Address::Region.find_by_code(province['regionCode'])
+      address_province.region = region
+      address_province.name = province['name']
+      address_province.save
     end
   end
 end
