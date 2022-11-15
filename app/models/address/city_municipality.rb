4f49fa0fb3a920bfbafa5e0112060ab3a56e5491
KodaCamp <>
2022-11-15 14:33:19 +0800

    remove unnecessary references

--- app/models/address/city_municipality.rb
+++ app/models/address/city_municipality.rb
@@ -2,7 +2,6 @@ class Address::CityMunicipality < ApplicationRecord
   validates :name, presence: true
   validates :code, uniqueness: true
 
-  belongs_to :region
   belongs_to :province, optional: true
   belongs_to :district, optional: true
   has_many :barangays
