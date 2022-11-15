44bc84165a5491b8311327dd8fc573429c380d07
KodaCamp <>
2022-11-15 18:02:04 +0800

    remove unnecessary controller

--- app/controllers/api/cities_municipalities_districts_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::CitiesMunicipalitiesDistrictsController < ApplicationController
-  def index
-    district = Address::District.find(params[:district_id])
-    city_municipalities = district.city_municipalities
-    render json: city_municipalities, each_serializer: CityMunicipalityDistrictSerializer
-  end
-end
--- app/controllers/api/cities_municipalities_provinces_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::CitiesMunicipalitiesProvincesController < ApplicationController
-  def index
-    province = Address::Province.find(params[:province_id])
-    city_municipalities = province.city_municipalities
-    render json: city_municipalities, each_serializer: CityMunicipalityProvinceSerializer
-  end
-end
