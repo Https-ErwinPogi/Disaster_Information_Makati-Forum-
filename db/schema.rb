fda482f677d1b8a19f512d3ce91eab8568895250
KodaCamp <>
2022-11-17 15:38:07 +0800

    add genre to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_072349) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_073500) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "code"
@@ -116,6 +116,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_072349) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
