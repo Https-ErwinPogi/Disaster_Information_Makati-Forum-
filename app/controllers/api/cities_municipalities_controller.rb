5434f21a3a1ca61c149beef8cfcdd0a1bfd292cd
KodaCamp <>
2022-11-18 18:10:26 +0800

    done city_municipality to hierarchy and modify city_municipalities controller

--- app/controllers/api/cities_municipalities_controller.rb
+++ app/controllers/api/cities_municipalities_controller.rb
@@ -1,13 +1,13 @@
 class Api::CitiesMunicipalitiesController < ApplicationController
   def index
-    district = Address::District.find_by_id(params[:district_id])
     province = Address::Province.find_by_id(params[:province_id])
-    if district
-      city_municipalities = district.city_municipalities
-      render json: city_municipalities, each_serializer: CityMunicipalityDistrictSerializer
-    else
+    if province
       city_municipalities = province.city_municipalities
       render json: city_municipalities, each_serializer: CityMunicipalityProvinceSerializer
+    else
+      district = Address::District.find_by_id(params[:province_id])
+      city_municipalities = district.city_municipalities
+      render json: city_municipalities, each_serializer: CityMunicipalityDistrictSerializer
     end
   end
 end
