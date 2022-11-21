77bf5eb305a1f85d4ccad481053b7d3abf962e43
KodaCamp <>
2022-11-21 16:29:50 +0800

    add config to admin controller, for pundit

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,24 @@
 class AdminController < ActionController::Base
   before_action :authenticate_user!
   before_action :check_admin
+  include Pundit
 
   def check_admin
     raise ActionController::RoutingError.new('Not Found') unless current_user.admin?
+  end
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:alert] = t "#{policy_name}.#{exception.query}",
+                      scope: 'pundit',
+                      default: :default,
+                      username: exception.policy.records&.user&.email
+    redirect_to(request.referrer || root_path)
+  end
+
+  def pundit_user
+    current_user
   end
 end
